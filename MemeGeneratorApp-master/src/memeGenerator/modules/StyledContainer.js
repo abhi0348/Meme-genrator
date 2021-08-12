9ecd525ffaadfae3e32e72137b8c6311d7918e15
Abhishek Yadav <ay0187900@gmail.com>
2021-08-12 14:12:34 +0530

    Delete MemeGeneratorApp-master directory

--- MemeGeneratorApp-master/src/memeGenerator/modules/StyledContainer.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledContainer = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-`;
